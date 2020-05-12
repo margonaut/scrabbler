5fcf10a5a926eb9211be817507f7eeaf0e352ef6
Margaret Morgan <margonaut@users.noreply.github.com>
2020-05-12 17:01:47 -0400

    Add and style WordSearchContainer component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -10,7 +10,6 @@ import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
 
 import Header from '../Header';
-import '../../styles/reset.css';
 import styles from './styles.css';
 
 const Layout = ({ children }) => {
