19f2c3d21536d3206c19ebb4e0e04fc464cc1a37
Bryn Morgan <bryn.p.morgan@icloud.com>
2020-05-19 21:02:14 -0400

    Added Link Redirect for search results

--- src/pages/search-results.js
+++ src/pages/search-results.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React, { useEffect } from 'react';
 
-import Layout from '../components/Layout';
 import Image from '../components/Image';
-import WordSearchContainer from '../components/WordSearchContainer';
+import Layout from '../components/Layout';
 import WordEvaluationDisplay from '../components/WordEvaluationDisplay';
+import WordSearchContainer from '../components/WordSearchContainer';
+import { navigate } from 'gatsby';
 
 const SearchResults = (props) => {
-  console.log(props);
-  return (
-    <Layout>
-      <WordSearchContainer></WordSearchContainer>
-      <WordEvaluationDisplay word={props.location.state.word}></WordEvaluationDisplay>
-    </Layout>
-  );
+  useEffect(() => {
+    if (!currentWord()) {
+      navigate('/');
+    }
+  });
+
+  const currentWord = () => location.state && location.state.word;
+
+  if (currentWord()) {
+    return (
+      <Layout>
+        <WordSearchContainer></WordSearchContainer>
+        <WordEvaluationDisplay
+          word={props.location.state.word}
+        ></WordEvaluationDisplay>
+      </Layout>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default SearchResults;
